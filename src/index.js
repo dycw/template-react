0fef686590a0a18c8490a20ff475f26e10d2957e
Derek Wan <d.wan@icloud.com>
2023-05-18 00:36:56 +0800

    Switch to `prettier-plugin-organize-imports` (#5)

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import React from "react";
-import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
